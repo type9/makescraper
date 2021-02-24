250ece6797a9e667bdc37157d7a81933aa71aee9
Gabriel Bernard Lee <gabriel@Gabriels-MacBook-Pro.local>
2021-02-24 14:39:02 +0800

    Completed makescraper

--- scrape.go
+++ scrape.go
@@ -1,23 +1,43 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/gocolly/colly"
 )
 
+type Headline struct {
+	Title  string
+	Author string
+}
+
 // main() contains code adapted from example found in Colly's docs:
 // http://go-colly.org/docs/examples/basic/
 func main() {
+	fName := "headlines.json"
+	file, err := os.Create(fName)
+	if err != nil {
+		log.Fatalf("Cannot create file %q: %s\n", fName, err)
+		return
+	}
+	defer file.Close()
 	// Instantiate default collector
 	c := colly.NewCollector()
 
-	// On every a element which has href attribute call callback
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-                link := e.Attr("href")
-
+	headlines := make([]Headline, 0, 100)
+	// On every title element
+	c.OnHTML(".c-card__header", func(e *colly.HTMLElement) {
+		author := e.ChildText(".c-card__byline")
+		headline := Headline{
+			e.Text,
+			author,
+		}
+		headlines = append(headlines, headline)
 		// Print link
-                fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+		fmt.Printf("Title Found: %q by %s\n", e.Text, author)
 	})
 
 	// Before making a request print "Visiting ..."
@@ -26,5 +46,11 @@ func main() {
 	})
 
 	// Start scraping on https://hackerspaces.org
-	c.Visit("https://hackerspaces.org/")
+	c.Visit("https://www.rollingstone.com/music/music-news/")
+
+	enc := json.NewEncoder(file)
+	enc.SetIndent("", "  ")
+
+	// Dump json to the standard output
+	enc.Encode(headlines)
 }
